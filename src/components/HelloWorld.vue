39c88d7d9bf19b534143bf9d58838961470b3059
Sunanth-Datasirpi <sunanth.h@datasirpi.com>
2024-03-11 22:53:54 +0530

    adding binding text, HTML, created new component

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,6 +2,11 @@
   <div>
     <h1>Hello World</h1>
     <h2>{{ msg }}</h2>
+    <h1> Hello {{ name }}</h1>
+    <h2>{{ greet }}</h2>
+    <h2 v-text="text"></h2>
+    <p v-html="html"></p>
+    
   </div>
 </template>
 
@@ -10,6 +15,14 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data(){
+    return{
+      name:"Sunanth",
+      greet:"Welcome to Vue",
+      text:"Binding text using v-text",
+      html:"<u><b>Binding html directly using v-html</b></u>",
+    }
   }
 }
 </script>
