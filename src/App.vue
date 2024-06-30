40f4fd1cb0473016bc971a85ebbc8dea6dc3f7d9
Sunanth-Datasirpi <sunanth.h@datasirpi.com>
2024-06-30 16:26:05 +0530

    adding the tutorial of event and event handling

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
   Event Bus Tutorial
   <event-bus-child-1></event-bus-child-1>
   <event-bus-child-2></event-bus-child-2>
+  <counter></counter>
 
 </div>
 </template>
@@ -10,12 +11,14 @@
 <script>
 import EventBusChild1 from '../src/components/EventBusChild1.vue';
 import EventBusChild2 from '../src/components/EventBusChild2.vue';
+import Counter from './components/Counter.vue';
 
 export default {
   name: 'App',
   components: {
     EventBusChild1,
     EventBusChild2,
+    Counter,
 
   }
 }
---
+++ src/components/Counter.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <div>
+      <h2>Counter: {{ count }}</h2>
+      <button @click="increment">Increment</button>
+      <button @click="decrement">Decrement</button>
+      <button @click="reset">Reset</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Counter-Event",
+  created() {},
+  data() {
+    return {
+      count: 0,
+    };
+  },
+  props: {},
+  methods: {
+    increment() {
+      this.count++;
+    },
+    decrement() {
+      if (this.count > 0) this.count--;
+    },
+    reset() {
+      this.count = 0;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
